26ee5027a8e2527982497e8dd088008f14710a34
8klancer <8klancer@gmail.com>
2022-11-23 21:49:14 +0400

    update privacyPolicy

--- pages/DexBattleTeam.js
+++ pages/DexBattleTeam.js
@@ -3,11 +3,6 @@ export default function Privacy() {
     return(
         <div className="w-screen mx-16 flex flex-col items-center">
             <h1 className="text-white font-Kanit text-5xl text-justify">Privacy Policy and Terms Conditions</h1>
-            <div>
-                {/*you will have a page like this*/}
-
-                <h1 className="text-gray-400 font-Kanit text-[65px]">Dex battle privacy policy page</h1>
-            </div>
         </div>
     )
 }
