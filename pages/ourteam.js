a3a4f2226f2e6dc51645d6be615fe298aee8d66b
8klancer <8klancer@gmail.com>
2022-11-25 22:02:09 +0400

    team page

---
+++ pages/ourteam.js
@@ -0,0 +1,14 @@
+import react from "react";
+
+
+function Ourteam () {
+    return (
+
+        <div className=" flex flex-col items-center">
+            <h1 className=" font-Kanit text-3xl text-center text-white"> Our Team</h1>
+
+        </div>
+    )
+}
+
+export default Ourteam
