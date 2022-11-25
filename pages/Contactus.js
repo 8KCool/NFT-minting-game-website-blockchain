d18db8578d3fa90b727ad339139cc03c662b5f61
8klancer <8klancer@gmail.com>
2022-11-25 22:35:16 +0400

    2.1

---
+++ pages/Contactus.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Contact () {
+    return (
+        <div className="flex flex-col items-center">
+            <h1 className="text-white text-3xl font-kanit"> contact us</h1>
+        </div>
+    )
+}
