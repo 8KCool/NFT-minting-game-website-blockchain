01775dcff1c3d85b2bae23a4f0d93326514a5a67
8klancer <8klancer@gmail.com>
2022-12-06 21:43:29 +0400

    adjust mobile view

--- components/Footer.js
+++ components/Footer.js
@@ -11,9 +11,9 @@ function Footer () {
               <div class="text-center items-center">
                   <h3 class="text-3xl mb-3"> Download DexBattle </h3>
                   <p> Make your own story. </p>
-                  <div class="flex justify-center my-10">
+                  <div class="md:flex justify-center my-10">
                     <a href={social.googlePlayStore}>
-                      <div class="md:flex items-center border rounded-lg px-4 py-2 w-52 mx-2">
+                      <div class="flex items-center border rounded-lg px-4 py-2 w-52 mx-2">
                           <img src="https://cdn-icons-png.flaticon.com/512/888/888857.png" class="w-7 md:w-8"/>
                           <div class="text-left ml-3">
                               <p class='text-xs text-gray-200'>Get it on </p>
