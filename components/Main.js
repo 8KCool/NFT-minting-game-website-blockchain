ca62e5b3493207065ba4f4c33fc8804c445ba7c6
8klancer <8klancer@gmail.com>
2022-12-10 21:52:43 +0400

    home page

--- components/Main.js
+++ components/Main.js
@@ -179,7 +179,7 @@ selling for millions of dollars, these fees may outweigh the revenue earned by i
              <img
             data-aos="flip-up"
             src='/DexBattle PNG/DexBattle-nostore.jpg'
-            className='md:mx-10 h-full w-full md:h-[70%] md:m-4 border border-gray-400 rounded-lg'/>
+            className='md:mx-10 h-full w-full md:w-[70%] border border-gray-400 rounded-lg'/>
 
             </div>
 
