f67a532507e60221b4f6460f12e9181b27558519
8klancer <8klancer@gmail.com>
2022-12-14 03:00:06 +0400

    reps page content

--- pages/buyThroughReps.js
+++ pages/buyThroughReps.js
@@ -3,8 +3,20 @@ import React from "react";
 export default function buyThroughReps () {
     return (
         <div>
-            <h1 className="font-Kanit text-40px text-center text-white">Buy through Our representatives</h1>
+            <h1 className="font-Kanit text-64px text-center text-white mt-8"> Our representatives</h1>
+
             {/*content*/}
+            <ul className="text-blue-400 font-Kanit text-50px max-h-screen justify-evenly flex flex-col md:mx-16">
+                <li>UAE</li>
+                <li>Morocco</li>
+                <li>Egypt</li>
+                <li>Turkey</li>
+                <li>Sri Lanka</li>
+                <li>Pakistan</li>
+            </ul>
         </div>
     )
 }
+
+
+
