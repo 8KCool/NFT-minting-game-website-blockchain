2351cab79d0521e8c6f384b7250c10b6acba13b0
8klancer <8klancer@gmail.com>
2022-12-23 10:22:53 +0400

    css fix

--- components/exchangetokens.js
+++ components/exchangetokens.js
@@ -7,6 +7,7 @@ import Footer from "../components/Footer"
 import {Link} from 'next/link';
 import Image from 'next/image';
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+import Navbar from './navExchange';
 
   const data = [
 	{ value: 30 },
@@ -148,104 +149,7 @@ useEffect(() => {
 return(
         <div className='w-full min-h-screen flex flex-col items-center justify-center overflow-hidden bg-gradient-to-br from-[#141414] to-[#330042]'>
       {/*navbar*/}
-      <div className='sticky top-0 w-[800px] mx-10 md:mx-10 lg:mx-10 bg-black/30 backdrop-blur-sm overflow-hidden border-b-white rounded-md my-4'>
-      <ul className='flex md:flex-row flex-col items-center items-center justify-between my-2'>
-        <li className='md:ml-10 lg:ml-10 ml-2'>
-          <a href='/'><img className = 'w-auto h-[55px]' 
-            src = '/Dex.png'
-          />
-          </a>
-        </li>
- 
-          <div className='hidden bg-gray-700/30 py-2 px-4 backdrop-blur-md md:flex flex-row font-Kanit text-white rounded-md text-[20px]'>
-          <a className="mx-4 cursor-pointer hover:text-blue-400" href='/'> <h1> Home</h1></a>
-            <a className="mx-4 cursor-pointer hover:text-blue-400" href='/mintingPortal'> <h1> Mint NFTs</h1></a>
-          </div>
-
-        <li className='hidden'>
-        
-            { walletAddress ?  (
-            <div className=' py-2 px-4 backdrop-blur-md flex flex-row font-Kanit text-white rounded-md border border-white bg-gradient-to-r from-green-300 via-blue-500 to-purple-600'>
-            <h1 className='mx-4 uppercase tracking-wide'>Connected: {walletAddress.slice(0, 8) + '...' + walletAddress.slice(-4)}</h1> </div>
-            ) : (
-            <div className=' py-2 px-4 backdrop-blur-md flex flex-row font-Kanit text-white rounded-md border border-white hover:bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 hover:text-black cursor-pointer' onClick={connectWalletHandler}>
-            <h1 className='mx-4 uppercase tracking-wide'>Connect Wallet</h1></div>
-            )
-            }
-
-          
-          
-        </li>
-        <li>
-        <div onClick={handleNav}
-          className=' md:hidden rounded-md p-2 bg-white mr-2 cursor-pointer' >
-            <AiOutlineMenu size={18} />
-          </div>
-        </li>
-
-      </ul>
-      
-      </div>
-
-                  {/* Mobile Menu */}
-      {/* Overlay */}
-      <div className={
-         nav? 'z-[120] md:hidden fixed left-0 top-0 w-full h-screen bg-gray-400/10 backdrop-filter backdrop-blur-sm' : ""
-        }>
-        
-        {/* Side Drawer Menu */}
-      <div className ={
-        nav? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-black p-10 ease-in duration-500 border-r-2"
-      :'fixed left-[-100%] top-0 p-10 ease-in duration-500 h-screen'
-      }>
-      <div className='flex w-full items-center justify-between'>
-      <div className="cursor-pointer">
-                <a>
-                  <Image
-                    src="/Dex.png"
-                    width='45'
-                    height='45'
-                    alt='/'
-                    className="rounded-md"
-                  />
-                </a>
-              </div>
-              <div onClick={handleNav}
-              className='fixed md:right-[-100%] rounded-full bg-gray-400 hover:shadow-lg hover:shadow-gray-600 p-3 cursor-pointer '>
-              <AiOutlineClose size={18} />
-             </div>
-          </div>
-          <div className='border-b py-4 flex flex-col mt-4'>
-              <p className='uppercase text-[18px] text-white font-Kanit text-center'>
-              Dex Battle Instant Exchange
-              </p>
-            </div>
-            <div  className='py-4 flex flex-col '>
-        <ul className='uppercase text-gray-200 font-Kanit text-center'>
-            <li  className='py-4 text-sm hover:text-white'>
-            <a onClick={() => setNav(false)} href='/'>Home</a>
-            </li>
-            <li  className='py-4 text-sm hover:text-white'>
-            <a onClick={() => setNav(false)} href='/mintingWithdex'>Buy NFTs</a>
-            </li>
-            <li>
-            <a className="cursor-pointe"  href='/DexBattleWhitepaper.pdf' download='DexBattleWhitepaper.pdf'>Whitepaper</a>
-            </li>
-            <li>
-        <div className='hidden py-2 px-4 backdrop-blur-md md:flex flex-row font-Kanit text-white rounded-md border border-white hover:bg-blue-200 hover:text-black'>
-            <a href='/mint'><h1 className='mx-4 uppercase tracking-wide hover:font-semibold'>Connect Wallet</h1> </a>
-
-          </div>
-          </li>
-            
-          </ul>
-
-        </div>
-        
-        </div>
-        
-
-      </div>
+    <Navbar/>
 
       {/*Title*/}
         <h1 className="tracking-wide font-Righteous uppercase font-bold text-3xl md:text-4xl text-brand-02 bg-clip-text mt-4 pt-3 ">
