6216c32892abadb3b171fe687ce883bfcfb07e8c
8klancer <8klancer@gmail.com>
2022-12-25 21:26:15 +0400

    css fix

--- components/Reps.js
+++ components/Reps.js
@@ -15,7 +15,7 @@ function Reps () {
     
             <div className="flex flex-col items-center bg-[#eee3d3] border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
                 <img className="object-cover border border-gray-900 w-full rounded-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg m-3" src="/placeholder.png" alt=""/>
-                    <div className="flex flex-col justify-between p-4 leading-normal">
+                    <div className="flex flex-col items-start">
                         <h5 className="mb-2 text-[40px] font-bold tracking-tight text-gray-900 dark:text-white">UAE</h5>
                         <h5 className="mb-2 text-[25px] font-bold tracking-tight text-gray-700 dark:text-white">Mohammed Fahmy</h5>
                         <div className="flex justify-between">
@@ -27,7 +27,7 @@ function Reps () {
 
             <div className="flex flex-col items-center bg-[#eee3d3] border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
                 <img className="object-cover border border-gray-900 w-full rounded-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg m-3" src="/placeholder.png" alt=""/>
-                    <div class="flex flex-col justify-between p-4 leading-normal">
+                    <div class="flex flex-col items-start">
                         <h5 className="mb-2 text-[40px] font-bold tracking-tight text-gray-900 dark:text-white">Morocco</h5>
                         <h5 className="mb-2 text-[25px] font-bold tracking-tight text-gray-700 dark:text-white">Abdeladim Nait Brahim</h5>
                         <div className="flex justify-between">
