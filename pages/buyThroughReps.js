f14ad6ea958fdd62423df2dc556eef1ec07a453f
8klancer <8klancer@gmail.com>
2022-12-25 20:24:43 +0400

    reps page css fix

--- pages/buyThroughReps.js
+++ pages/buyThroughReps.js
@@ -12,7 +12,7 @@ export default function buyThroughReps () {
       </Head>
 
       <Navbar/>
-            <h1 className="font-Kanit md:text-[64px] text-[40px] text-center text-white mt-8"> Our representatives</h1>
+            <h1 className="font-Kanit md:text-[64px] text-[40px] text-center text-white mt-[100px]"> Our representatives</h1>
 
             {/*content*/}
 
@@ -22,17 +22,17 @@ export default function buyThroughReps () {
                     
                     <div>
                         
-                        <a href="#" class="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                            <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/placeholder.png" alt=""/>
+                        <div class="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                            <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg m-3" src="/placeholder.png" alt=""/>
                                 <div class="flex flex-col justify-between p-4 leading-normal">
-                                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">UAE</h5>
-                                    <h5 class="mb-2 text-[40px] font-bold tracking-tight text-gray-900 dark:text-white">Name here</h5>
+                                    <h5 class="mb-2 text-[40px] font-bold tracking-tight text-gray-900 dark:text-white">UAE</h5>
+                                    <h5 class="mb-2 text-[30px] font-bold tracking-tight text-gray-700 dark:text-white">Name here</h5>
                                     <div className="flex justify-between">
-                                        <img src="/whatsapp.svg h-[40px] w-[40px]"/>
+                                        <img src="/whatsapp.svg" classname='h-[40px] w-[40px]'/>
                                         <hl className='font-Kanit text-[30px]'>number here</hl>
                                     </div>
                                 </div>
-                        </a>
+                        </div>
 
 
                     </div>
