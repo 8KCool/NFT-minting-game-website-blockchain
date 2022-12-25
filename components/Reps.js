72ffe2ddf8a4b6e05b256b4721a7b3f647c73120
8klancer <8klancer@gmail.com>
2022-12-25 22:44:36 +0400

    css fix

--- components/Reps.js
+++ components/Reps.js
@@ -14,10 +14,10 @@ function Reps () {
 <div className="text-blue-400 font-Kanit text-50px max-h-screen justify-evenly flex flex-col md:mx- mx-2">
     
         
-        <div className='flex md:flex-row flex-col md:my-8 my-2'>
+        <div className='flex flex-row  md:my-8 my-2'>
     
             <div className="flex flex-col items-center bg-[#eee3d3] border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray- md:mx-8 mx-2">
-                <img className="object-cover border border-gray-900 w-full rounded-lg h-96 md:h-auto md:w-48 m-3" src="/placeholder.png" alt=""/>
+                <img className="object-cover border border-gray-900 w-full rounded-lg h-48 md:h-auto md:w-48 m-3" src="/placeholder.png" alt=""/>
                     <div className="flex flex-col items-start mx-4">
                         <h5 className="mb-2 text-[40px] font-bold tracking-tight text-gray-900 dark:text-white">UAE</h5>
                         <h5 className="mb-2 text-[25px] font-bold tracking-tight text-gray-700 dark:text-white">Mohammed Fahmy</h5>
@@ -29,7 +29,7 @@ function Reps () {
             </div>
 
             <div className="flex flex-col items-center bg-[#eee3d3] border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 md:mx-8 mx-2">
-                <img className="object-cover border border-gray-900 w-full rounded-lg h-96 md:h-auto md:w-48 m-3" src="/placeholder.png" alt=""/>
+                <img className="object-cover border border-gray-900 w-full rounded-lg h-48 md:h-auto md:w-48 m-3" src="/placeholder.png" alt=""/>
                     <div class="flex flex-col items-start mx-4">
                         <h5 className="mb-2 text-[40px] font-bold tracking-tight text-gray-900 dark:text-white">Morocco</h5>
                         <h5 className="mb-2 text-[25px] font-bold tracking-tight text-gray-700 dark:text-white">Abdeladim Nait Brahim</h5>
@@ -41,10 +41,10 @@ function Reps () {
             </div>
             </div>
 
-            <div className='flex md:flex-row flex-col md:mt-8 mt-4'>
+            <div className='flex flex-row  md:mt-8 mt-4'>
 
             <div className="flex flex-col items-center bg-[#eee3d3] border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 md:mx-8 mx-2">
-                <img className="object-cover border border-gray-900 w-full rounded-lg h-96 md:h-auto md:w-48 m-3" src="/placeholder.png" alt=""/>
+                <img className="object-cover border border-gray-900 w-full rounded-lg h-48 md:h-auto md:w-48 m-3" src="/placeholder.png" alt=""/>
                     <div className="flex flex-col items-start mx-4">
                         <h5 className="mb-2 text-[40px] font-bold tracking-tight text-gray-900 dark:text-white">Turkey</h5>
                         <h5 className="mb-2 text-[25px] font-bold tracking-tight text-gray-700 dark:text-white">Nadir Emre Yenigün</h5>
@@ -56,7 +56,7 @@ function Reps () {
             </div>
 
             <div className="flex flex-col items-center bg-[#eee3d3] border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 md:mx-8 mx-2">
-                <img className="object-cover border border-gray-900 w-full rounded-lg h-96 md:h-auto md:w-48 m-3" src="/placeholder.png" alt=""/>
+                <img className="object-cover border border-gray-900 w-full rounded-lg h-48 md:h-auto md:w-48 m-3" src="/placeholder.png" alt=""/>
                     <div className="flex flex-col items-start mx-4">
                         <h5 className="mb-2 text-[40px] font-bold tracking-tight text-gray-900 dark:text-white">Egypt</h5>
                         <h5 className="mb-2 text-[25px] font-bold tracking-tight text-gray-700 dark:text-white">Magdy Ahmed El Zanaty</h5>
