7d39f183d41694ae74ce56c82f4b125d1d1a3d75
8klancer <8klancer@gmail.com>
2022-12-25 21:07:50 +0400

    css fix

--- components/Reps.js
+++ components/Reps.js
@@ -14,10 +14,10 @@ function Reps () {
         <div className='grid grid-cols-1 md:grid-cols-2 gap-5 my-8'>
     
             <div class="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg m-3" src="/placeholder.png" alt=""/>
+                <img class="object-cover border border-gray-900 w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg m-3" src="/placeholder.png" alt=""/>
                     <div class="flex flex-col justify-between p-4 leading-normal">
                         <h5 class="mb-2 text-[40px] font-bold tracking-tight text-gray-900 dark:text-white">UAE</h5>
-                        <h5 class="mb-2 text-[30px] font-bold tracking-tight text-gray-700 dark:text-white">Mohammed Fahmy</h5>
+                        <h5 class="mb-2 text-[25px] font-bold tracking-tight text-gray-700 dark:text-white">Mohammed Fahmy</h5>
                         <div className="flex justify-between">
                             <img src="/whatsapp.png" className='h-[50px] w-[50px]'/>
                             <hl className='font-Kanit text-[30px]'>+971554740803</hl>
@@ -26,10 +26,10 @@ function Reps () {
             </div>
 
             <div class="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg m-3" src="/placeholder.png" alt=""/>
+                <img class="object-cover border border-gray-900 w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg m-3" src="/placeholder.png" alt=""/>
                     <div class="flex flex-col justify-between p-4 leading-normal">
                         <h5 class="mb-2 text-[40px] font-bold tracking-tight text-gray-900 dark:text-white">Morocco</h5>
-                        <h5 class="mb-2 text-[30px] font-bold tracking-tight text-gray-700 dark:text-white">Abdeladim Nait Brahim</h5>
+                        <h5 class="mb-2 text-[25px] font-bold tracking-tight text-gray-700 dark:text-white">Abdeladim Nait Brahim</h5>
                         <div className="flex justify-between">
                             <img src="/whatsapp.png" className='h-[50px] w-[50px]'/>
                             <hl className='font-Kanit text-[30px]'>+212636886027</hl>
@@ -38,10 +38,10 @@ function Reps () {
             </div>
 
             <div class="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg m-3" src="/placeholder.png" alt=""/>
-                    <div class="flex flex-col justify-between p-4 leading-normal">
+                <img class="object-cover border border-gray-900 w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg m-3" src="/placeholder.png" alt=""/>
+                    <div class="flex flex-col items-start">
                         <h5 class="mb-2 text-[40px] font-bold tracking-tight text-gray-900 dark:text-white">Turkey</h5>
-                        <h5 class="mb-2 text-[30px] font-bold tracking-tight text-gray-700 dark:text-white">Nadir Emre Yenigün</h5>
+                        <h5 class="mb-2 text-[25px] font-bold tracking-tight text-gray-700 dark:text-white">Nadir Emre Yenigün</h5>
                         <div className="flex justify-between">
                             <img src="/whatsapp.png" className='h-[50px] w-[50px]'/>
                             <hl className='font-Kanit text-[30px]'>+905354199983</hl>
@@ -50,10 +50,10 @@ function Reps () {
             </div>
 
             <div class="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg m-3" src="/placeholder.png" alt=""/>
-                    <div class="flex flex-col justify-between p-4 leading-normal">
+                <img class="object-cover border border-gray-900 w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg m-3" src="/placeholder.png" alt=""/>
+                    <div class="flex flex-col items-start">
                         <h5 class="mb-2 text-[40px] font-bold tracking-tight text-gray-900 dark:text-white">Egypt</h5>
-                        <h5 class="mb-2 text-[30px] font-bold tracking-tight text-gray-700 dark:text-white">Magdy Ahmed El Zanatyn</h5>
+                        <h5 class="mb-2 text-[25px] font-bold tracking-tight text-gray-700 dark:text-white">Magdy Ahmed El Zanaty</h5>
                         <div className="flex justify-between">
                             <img src="/whatsapp.png" className='h-[50px] w-[50px]'/>
                             <hl className='font-Kanit text-[30px]'>+20100633105</hl>
