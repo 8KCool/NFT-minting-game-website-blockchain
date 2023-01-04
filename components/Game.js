0cca506198be6b7064991cbe096fef743d9b8e06
8klancer <8klancer@gmail.com>
2023-01-04 23:46:31 +0400

    css fix

--- components/Game.js
+++ components/Game.js
@@ -685,15 +685,15 @@ character selection and it would be your chance to feel yourself unbeatable.</li
 
   {/* content here */}
   <div className='w-full flex flex-col items-center justify-between overflow-hidden my-4'> 
-  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 ">
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 ">
 {
  Weaplist.map((item) =>
  <div key={item.id} item={item} class="my-4 group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
- <div class="h-72 w-96">
+ <div class="h-[300px] w-[400px]">
    <img class="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125" src={item.image} alt="Weapon" />
  </div>
  <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
- <div class="absolute inset-0 flex translate-y-[90%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+ <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
    <h1 class="font-Kanit text-3xl font-bold text-white">{item.title}</h1>
    <ol class="mb-3 text-[15px] italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
        <li>Type:{item.info} </li>
