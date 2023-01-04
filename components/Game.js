2d35c801735cd93a8c14744a958ed980c2b8461a
8klancer <8klancer@gmail.com>
2023-01-04 23:41:27 +0400

    css fix

--- components/Game.js
+++ components/Game.js
@@ -693,7 +693,7 @@ character selection and it would be your chance to feel yourself unbeatable.</li
    <img class="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125" src={item.image} alt="Weapon" />
  </div>
  <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
- <div class="absolute inset-0 flex translate-y-[80%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+ <div class="absolute inset-0 flex translate-y-[90%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
    <h1 class="font-Kanit text-3xl font-bold text-white">{item.title}</h1>
    <ol class="mb-3 text-[15px] italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
        <li>Type:{item.info} </li>
