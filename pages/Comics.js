69fe4e7dac90a5b0ab2a80018f82c92f0bb2a2b6
8klancer <8klancer@gmail.com>
2023-01-06 14:49:37 +0400

    comics page

--- pages/Comics.js
+++ pages/Comics.js
@@ -1,10 +1,15 @@
 import Navbar from '../components/NavAbout'
 export default function Comics(){
     return(
-        <div>
+        <div className='overflow-hidden'>
             <Navbar/>
             <div  className='flex flex-col items-center md:px-16 px-4 overflow-hidden'>
-            <h1 className='font-Kanit text-2xl md:text-3xl uppercase text-blue-400  mt-[100px]'>Comics</h1>
+            <h1 className='font-Kanit text-2xl md:text-4xl text-blue-400  mt-[100px]'>Comics</h1>
+
+            <div className='w-full h-auto md:my-6 my-2 flex flex-col items-center justify-start'>
+                <img src='/comicCover.png' className='w-full h-full filter blur-sm object-cover absolute'/>
+                <h1 className='text-white font-Kanit text-4xl text-center relative'></h1>
+            </div>
             </div>
         </div>
     )
