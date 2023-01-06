4fb27ff35aa582ab5d5006a1ba2d80c34a89a114
8klancer <8klancer@gmail.com>
2023-01-06 14:28:12 +0400

    css fix

--- pages/Comics.js
+++ pages/Comics.js
@@ -3,8 +3,8 @@ export default function Comics(){
     return(
         <div>
             <Navbar/>
-            <div  className='flex flex-col items-center md:px-16 px-4 overflow-hidden mt-[100px]'>
-            <h1 className='font-Kanit text-2xl md:text-3xl uppercase text-blue-400'>Comics</h1>
+            <div  className='flex flex-col items-center md:px-16 px-4 overflow-hidden'>
+            <h1 className='font-Kanit text-2xl md:text-3xl uppercase text-blue-400  mt-[100px]'>Comics</h1>
             </div>
         </div>
     )
