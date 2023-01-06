7ff5275e277440400955358aff891e21b28b56d2
8klancer <8klancer@gmail.com>
2023-01-06 14:26:39 +0400

    css fix

--- pages/Comics.js
+++ pages/Comics.js
@@ -1,9 +1,9 @@
 import Navbar from '../components/NavAbout'
 export default function Comics(){
     return(
-        <div className='flex flex-col items-center md:px-16 px-4'>
+        <div>
             <Navbar/>
-            <div className='mt-[100px]'>
+            <div  className='flex flex-col items-center md:px-16 px-4 overflow-hidden mt-[100px]'>
             <h1 className='font-Kanit text-2xl md:text-3xl uppercase text-blue-400'>Comics</h1>
             </div>
         </div>
