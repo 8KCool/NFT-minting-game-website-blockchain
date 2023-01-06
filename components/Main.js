2d2b7564bfd41cd0264040f7d04946155484eff0
8klancer <8klancer@gmail.com>
2023-01-06 12:02:25 +0400

    react player

--- components/Main.js
+++ components/Main.js
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import Aos from 'aos';
 import "aos/dist/aos.css";
 import {social} from "../info.config";
+import ReactPlayer from 'react-player/youtube';
 
 export default function Main() {
 
