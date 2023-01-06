c2b8dbb25240ba194716a0820347d9b54819f263
8klancer <8klancer@gmail.com>
2023-01-06 15:32:59 +0400

    rcss fix

--- pages/Comics.js
+++ pages/Comics.js
@@ -6,8 +6,8 @@ export default function Comics(){
             <div  className='flex flex-col items-center md:mx-16 mx-4 overflow-hidden'>
             <h1 className='font-Kanit text-2xl md:text-[60px] text-blue-400  mt-[100px] mb-4'>Comics</h1>
                 
-                <img src='comicCover.png' className='w-[800px] h-[800px] mt-8 filter blur-md absolute'/>
-                <div className='flex flex-col items-center justify-center relative top-10'>
+                <img src='comicCover.png' className='w-[800px] h-[800px] mt-8 filter blur-sm rounded-sm border border-gray-300 absolute'/>
+                <div className='flex flex-col items-center justify-center relative top-2'>
                     <h1 className='font-Kanit text-[48px] text-center py-2 px-4 text-white font-medium tracking-wider border boder-white rounded-md'>Comming Soon!</h1>
                 </div>
             </div>
