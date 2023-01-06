acd93269e1ab9dc9b5dfe738636e770d88724037
8klancer <8klancer@gmail.com>
2023-01-06 15:43:12 +0400

    css fix

--- pages/Comics.js
+++ pages/Comics.js
@@ -4,7 +4,7 @@ export default function Comics(){
         <div className='overflow-x-hidden'>
             <Navbar/>
             <div  className='flex flex-col items-center md:mx-16 mx-4 overflow-hidden'>
-            <h1 className='font-Kanit text-2xl md:text-[60px] text-[48px] text-blue-400  mt-[100px] mb-4'>Comics</h1>
+            <h1 className='font-Kanit text-2xl text-[60px] text-blue-400  mt-[100px] mb-4'>Comics</h1>
                 
                 
                 <img src='comicCover.png' className='md:w-[800px] md:h-[800px] lg:w-[800px] lg:h-[800px] w-[400px] h-[400px] mt-8 filter blur-sm rounded-md border border-gray-300 absolute'/>
