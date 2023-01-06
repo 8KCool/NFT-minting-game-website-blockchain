8e2b94a25fcdd4b2053f1a93c763c3884099fb56
8klancer <8klancer@gmail.com>
2023-01-06 15:38:16 +0400

    css fix

--- pages/Comics.js
+++ pages/Comics.js
@@ -6,11 +6,11 @@ export default function Comics(){
             <div  className='flex flex-col items-center md:mx-16 mx-4 overflow-hidden'>
             <h1 className='font-Kanit text-2xl md:text-[60px] text-blue-400  mt-[100px] mb-4'>Comics</h1>
                 
-                <div className='rounded-md border border-gray-300'>
-                <img src='comicCover.png' className='w-[800px] h-[800px] mt-8 filter blur-sm absolute'/>
-                <div className='flex flex-col items-center justify-center relative top-1'>
+                
+                <img src='comicCover.png' className='w-[800px] h-[800px] mt-8 filter blur-sm rounded-md border border-gray-300 absolute'/>
+                <div className='flex flex-col items-center justify-center relative bottom-1'>
                     <h1 className='font-Kanit text-[48px] text-center py-2 px-4 text-white font-medium tracking-wider border boder-white rounded-md'>Comming Soon!</h1>
-                </div>
+                
                 </div>
             </div>
         </div>
