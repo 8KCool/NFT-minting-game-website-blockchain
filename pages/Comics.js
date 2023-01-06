4b069dde65e1edc47e7e8b136eed6f0f8bd7f0b9
8klancer <8klancer@gmail.com>
2023-01-06 15:17:21 +0400

    css fix

--- pages/Comics.js
+++ pages/Comics.js
@@ -1,17 +1,12 @@
 import Navbar from '../components/NavAbout'
 export default function Comics(){
     return(
-        <div className='overflow-hidden'>
+        <div className='overflow-y-hidden'>
             <Navbar/>
             <div  className='flex flex-col items-center md:mx-16 mx-4 overflow-hidden'>
             <h1 className='font-Kanit text-2xl md:text-[60px] text-blue-400  mt-[100px] mb-4'>Comics</h1>
-
-            <div className='w-full h-auto md:my-6 my-2 md:px-16 px-4 flex flex-col items-center justify-start'>
-                <img src='/comicCover.png' className='w-full h-full filter blur-sm object-cover absolute'/>
-                <div className='flex flex-col items-center justify-center relative top-10'>
-                <h1 className='text-white font-Kanit text-[48px] text-centertop-10 px-4 py-2 border border-white rounded-md'> Coming Soon!</h1>
-                </div>
-            </div>
+                
+                <img src='comicCover.png' className='w-[800px] h-[800px] mt-8'/>
             </div>
         </div>
     )
