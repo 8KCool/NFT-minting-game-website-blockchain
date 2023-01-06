dc10d9e743ed6e29c5766ec6de15560edd31810d
8klancer <8klancer@gmail.com>
2023-01-06 15:31:31 +0400

    head fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <div>
+
+      <Head>
+        <title>DexBattle</title>
+        <meta name="Description" content="Dex Battle Game!" />
+        <link rel="icon" href="/Dex.png" />
+      </Head>
+    <Component {...pageProps}/>
+   </div>
 }
 
 export default MyApp
